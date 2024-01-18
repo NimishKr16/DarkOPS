80d17b3a23e59769f8560785bcf97513e701e912
Nimish Kumar <140400183+NimishKr16@users.noreply.github.com>
2024-01-18 14:28:25 +0530

    server.py

--- Extension/server.py
+++ Extension/server.py
@@ -1,16 +1,27 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from aichecker import aichecker
+from urlscanner import IPQS
 
 app = Flask(__name__)
 CORS(app)
 
+strictness = 0
+
+    # custom feilds
+additional_params = {
+        'strictness': strictness
+    }
 
 @app.route("/ff", methods=["POST"])
 def process_url():
     data = request.get_json()
-    url = data.get("url", "URL not found")
-    result_message = f"URL: {url.upper()}\nURL length : {len(url)}"
-    return jsonify({"message": result_message})
+    check_URL = data.get("url", "URL not found")
+    # result_message = f"URL: {url.upper()}\nURL length : {len(url)}"
+    ipqs = IPQS()
+    res = ipqs.malicious_url_scanner_api(url = check_URL)
+    result_message = "Risk Score: " +  str(res['risk_score'])
+    return jsonify({"message": result_message})   
 
 
 if __name__ == "__main__":
